a7a0cf45f70504c1a3c320ee4788fb50650eb5bb
vinnyegon90 <vinnyegon90@gmail.com>
2024-04-21 01:20:37 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, request,render_template, redirect,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from passlib.hash import sha256_crypt
 import datetime
+import bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -32,6 +33,7 @@ def article_to_dict(article):
         'body': article.body,
         'date': article.date.strftime('%Y-%m-%d %H:%M:%S')
     }
+
 
 @app.route('/get', methods=['GET'])
 def get_articles():
