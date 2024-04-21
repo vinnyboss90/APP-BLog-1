98999b09baffc81c53a3cbf18a3b53a2fe7205be
vinnyegon90 <vinnyegon90@gmail.com>
2024-04-22 02:08:01 +0300

    html

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use("/", (req, res) => {
-    res.send("Server is running.");
-});
-
-app.listen(5000, console.log("Server started on PORT 5000"));
